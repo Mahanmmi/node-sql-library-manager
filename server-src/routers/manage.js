9e1c92caa29e4bcac291a27879850cf806ec6281
mahanmmi <mahanmmi@gmail.com>
2021-02-03 22:52:47 +0330

    search users  added

--- server-src/routers/manage.js
+++ server-src/routers/manage.js
@@ -25,13 +25,28 @@ router.get('/borrowedbookreport', userAuth, async (req, res) => {
 router.delete('/user', userAuth, async (req, res) =>{
   const pool = await getPool();
   try {
-    const result = await pool.query(adminQueries.deleteUser, [
+    await pool.query(adminQueries.deleteUser, [
       req.body.username,
+      req.user.user_type,
+    ]);
+    return res.sendStatus(200);
+  } catch (err) {
+    return res.status(400).send(`Delete user failed: ${err.message}`);
+  }
+});
+
+router.get('/user', userAuth, async (req, res) => {
+  const pool = await getPool();
+  try {
+    const result = await pool.query(adminQueries.searchUsers, [
+      req.query.username ? `%${req.query.username}%` : '%',
+      req.query.lastName ? `%${req.query.lastName}%` : '%',
+      req.query.page ? req.query.page : 0,
       req.user.user_type,
     ]);
     return res.send(result);
   } catch (err) {
-    return res.status(400).send(`Delete user failed: ${err.message}`);
+    return res.status(400).send(`Search user failed: ${err.message}`);
   }
 });
 
--- server-src/sql/adminQueries.js
+++ server-src/sql/adminQueries.js
@@ -40,6 +40,39 @@ END;
 $delete_user_function$ LANGUAGE plpgsql;
 `.trim();
 
+const registerUserSearchFunction = `
+CREATE OR REPLACE FUNCTION user_search_function(
+  in_username text,
+  in_lastname text,
+  in_page INT,
+  in_user_type user_mods
+) RETURNS TABLE (
+    user_name citext,
+    password text,
+    first_name text,
+    lastname text,
+    user_type user_mods,
+    user_balance REAL,
+    created_on TIMESTAMPTZ
+) AS $user_search_function$
+BEGIN
+IF in_user_type != 'manager' AND in_user_type != 'librarian' THEN
+    RAISE EXCEPTION 'You dont have permission to search users';
+END IF;
+RETURN QUERY (
+    SELECT *
+    FROM Users
+    WHERE
+      username like in_username AND
+      last_name like in_lastname
+    ORDER BY last_name
+    LIMIT 5
+    OFFSET 5*in_page
+  );
+END;
+$user_search_function$ LANGUAGE plpgsql;
+`.trim();
+
 const getBorrowedBookReport = `
 SELECT get_borrowed_book_function(
   $1, $2
@@ -52,13 +85,21 @@ SELECT delete_user_function(
 );
 `.trim();
 
+const searchUsers = `
+SELECT user_search_function(
+  $1, $2, $3, $4
+);
+`.trim();
+
 async function registerAdminFunctions(client) {
   await client.query(registerGetBorrowedBooksFunction);
   await client.query(registerDeleteUserFunction);
+  await client.query(registerUserSearchFunction);
 }
 
 module.exports = {
   registerAdminFunctions,
   getBorrowedBookReport,
   deleteUser,
+  searchUsers,
 };
